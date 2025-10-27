9fc9fede24b25d155488eed8fbac03be0485fe0e
Mohd Zaid Sarwar <140310771+imzaidsarwar@users.noreply.github.com>
2025-10-27 22:43:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (94.21%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        
+        while (b != 0)
+        {
+
+            if(b > 0)
+            {
+                
+                a++;
+                b--;
+            }
+
+            else
+            {
+                a--;
+                b++;
+            }
+        }
+        
+        return a;
+    }
+};
